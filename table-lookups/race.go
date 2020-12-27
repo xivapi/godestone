95d0da5120f9df6ab0c25bc99b319c17f4ed3fab
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-12-27 11:24:59 -0800

    Fix character fetching in French

--- table-lookups/race.go
+++ table-lookups/race.go
@@ -15,17 +15,32 @@ func RaceTableLookup(raceTable *exports.RaceTable, name string) *exports.Race {
 		race := exports.Race{}
 		raceTable.Races(&race, i)
 
-		nameEn := string(race.NameEn())
-		nameDe := string(race.NameDe())
-		nameFr := string(race.NameFr())
-		nameJa := string(race.NameJa())
+		nameMasculineEn := string(race.NameMasculineEn())
+		nameMasculineDe := string(race.NameMasculineDe())
+		nameMasculineFr := string(race.NameMasculineFr())
+		nameMasculineJa := string(race.NameMasculineJa())
+		nameFeminineEn := string(race.NameFeminineEn())
+		nameFeminineDe := string(race.NameFeminineDe())
+		nameFeminineFr := string(race.NameFeminineFr())
+		nameFeminineJa := string(race.NameFeminineJa())
 
-		nameEnLower := strings.ToLower(nameEn)
-		nameDeLower := strings.ToLower(nameDe)
-		nameFrLower := strings.ToLower(nameFr)
-		nameJaLower := strings.ToLower(nameJa)
+		nameMasculineEnLower := strings.ToLower(nameMasculineEn)
+		nameMasculineDeLower := strings.ToLower(nameMasculineDe)
+		nameMasculineFrLower := strings.ToLower(nameMasculineFr)
+		nameMasculineJaLower := strings.ToLower(nameMasculineJa)
+		nameFeminineEnLower := strings.ToLower(nameFeminineEn)
+		nameFeminineDeLower := strings.ToLower(nameFeminineDe)
+		nameFeminineFrLower := strings.ToLower(nameFeminineFr)
+		nameFeminineJaLower := strings.ToLower(nameFeminineJa)
 
-		if nameEnLower == nameLower || nameDeLower == nameLower || nameFrLower == nameLower || nameJaLower == nameLower {
+		if nameMasculineEnLower == nameLower ||
+			nameMasculineDeLower == nameLower ||
+			nameMasculineFrLower == nameLower ||
+			nameMasculineJaLower == nameLower ||
+			nameFeminineEnLower == nameLower ||
+			nameFeminineDeLower == nameLower ||
+			nameFeminineFrLower == nameLower ||
+			nameFeminineJaLower == nameLower {
 			return &race
 		}
 	}
