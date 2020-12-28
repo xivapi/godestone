d206eeaf7320474333d17b1745e2141f9737a9aa
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-12-28 14:16:51 -0800

    Clean up MiMo
    Former-commit-id: dd44c136ecd3845157d21bc93e6153d5042a06eb

--- collectors/mount.go
+++ collectors/mount.go
@@ -1,7 +1,7 @@
 package collectors
 
 import (
-	"strings"
+	lookups "github.com/karashiiro/godestone/table-lookups"
 
 	"github.com/gocolly/colly/v2"
 	"github.com/karashiiro/godestone/models"
@@ -19,37 +19,18 @@ func BuildMountCollector(meta *models.Meta, profSelectors *selectors.ProfileSele
 
 	c.OnHTML(mountSelectors.Mounts.Root.Selector, func(e *colly.HTMLElement) {
 		name := mountSelectors.Mounts.Name.ParseThroughChildren(e)[0]
-		nameLower := strings.ToLower(name)
-
 		icon := mountSelectors.Mounts.Icon.ParseThroughChildren(e)[0]
 
-		nMounts := mountTable.MountsLength()
-		for i := 0; i < nMounts; i++ {
-			mount := exports.Mount{}
-			mountTable.Mounts(&mount, i)
+		m := lookups.MountTableLookup(mountTable, name)
+		output <- &models.Mount{
+			ID:   m.Id(),
+			Name: name,
+			Icon: icon,
 
-			nameEn := string(mount.NameEn())
-			nameDe := string(mount.NameDe())
-			nameFr := string(mount.NameFr())
-			nameJa := string(mount.NameJa())
-
-			nameEnLower := strings.ToLower(nameEn)
-			nameDeLower := strings.ToLower(nameDe)
-			nameFrLower := strings.ToLower(nameFr)
-			nameJaLower := strings.ToLower(nameJa)
-
-			if nameEnLower == nameLower || nameDeLower == nameLower || nameFrLower == nameLower || nameJaLower == nameLower {
-				output <- &models.Mount{
-					ID:   mount.Id(),
-					Name: name,
-					Icon: icon,
-
-					NameEN: nameEn,
-					NameDE: nameDe,
-					NameFR: nameFr,
-					NameJA: nameJa,
-				}
-			}
+			NameEN: string(m.NameEn()),
+			NameDE: string(m.NameDe()),
+			NameFR: string(m.NameFr()),
+			NameJA: string(m.NameJa()),
 		}
 	})
 
