81b7773060e94685d2de3b0071d32675ff60d0ba
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-12-28 10:00:33 -0800

    Some better docs

--- lang.go
+++ lang.go
@@ -3,7 +3,9 @@ package godestone
 // SiteLang represents the scraped website language.
 type SiteLang string
 
-// Lodestone website language.
+// Lodestone website language. Do note that all five language-versions of the website (`eu` not listed) are on
+// the same physical servers in Japan. Changing the language of the website will not meaningfully improve
+// response times.
 const (
 	JA SiteLang = "jp"
 	EN SiteLang = "na"
