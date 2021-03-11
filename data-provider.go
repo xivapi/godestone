101c36d9e93c44480847ef17783cc7991d8cab44
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-03-10 18:13:24 -0800

    prototype DataProvider interface

---
+++ data-provider.go
@@ -0,0 +1,22 @@
+package godestone
+
+import (
+	"github.com/karashiiro/godestone/models"
+	"github.com/karashiiro/godestone/pack/exports"
+)
+
+// DataProvider represents a service implementation responsible for
+// converting our scraped data into a useful form.
+type DataProvider interface {
+	ClassJob(name string) models.ClassJob
+	Deity(name string) models.NamedEntity
+	GrandCompany(name string) models.NamedEntity
+	Item(name string) exports.Item    // Do something about this
+	Minion(name string) models.Minion // Or all of these
+	Mount(name string) models.Mount
+	Race(name string) models.GenderedEntity
+	Reputation(name string) models.NamedEntity
+	Title(name string) models.Title
+	Town(name string) models.NamedEntity
+	Tribe(name string) models.GenderedEntity
+}
